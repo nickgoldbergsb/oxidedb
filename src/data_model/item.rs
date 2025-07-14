0e866012a04d53a26356cd3639af097a1c92ac71
nicholas.goldberg <nicholas.goldberg@cox.net>
2025-07-14 14:54:28 -0700

    expanded filter and accidentally formatted

--- src/data_model/item.rs
+++ src/data_model/item.rs
@@ -9,19 +9,27 @@ pub struct Item {
 }
 
 impl Item {
-    pub fn new(id: String, vector: Vector, metadata: Option<HashMap<String, MetadataValue>>) -> Self {
-        Self { id, vector, metadata }
+    pub fn new(
+        id: String,
+        vector: Vector,
+        metadata: Option<HashMap<String, MetadataValue>>,
+    ) -> Self {
+        Self {
+            id,
+            vector,
+            metadata,
+        }
     }
 
-    pub fn get_id(&self) -> &String {
+    pub fn id(&self) -> &str {
         &self.id
     }
 
-    pub fn get_vector(&self) -> &Vector {
+    pub fn vector(&self) -> &Vector {
         &self.vector
     }
 
-    pub fn get_metadata(&self) -> Option<&HashMap<String, MetadataValue>> {
+    pub fn metadata(&self) -> Option<&HashMap<String, MetadataValue>> {
         self.metadata.as_ref()
     }
 }
--- src/data_model/mod.rs
+++ src/data_model/mod.rs
@@ -1,5 +1,5 @@
-pub mod vector;
-pub mod item;
-pub mod store;
-pub mod metrics;
 pub mod filter;
+pub mod item;
+pub mod metrics;
+pub mod store;
+pub mod vector;
